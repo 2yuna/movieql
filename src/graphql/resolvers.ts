dcba78f4285c514d59c28e4ed49ade8e7d4d435e
Yuna Lee <y.duck10002@gmail.com>
2021-04-04 17:31:17 +0900

    #9 Creating first Mutation

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,10 +1,13 @@
 import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
-import { getById, getMovies } from "./db";
+import { addMovie, getById, getMovies } from "./db";
 
 const resolvers = {
   Query: {
     movies: () => getMovies(),
     movie: (_, { id }) => getById(id),
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
   },
 };
 
